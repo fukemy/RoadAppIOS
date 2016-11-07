3219d2fee229099829a0d2ce143dea860bfa8365
fukemy <fukemy@yahoo.com>
2016-11-07 18:42:08 +0700

    Initial Commit

---
+++ RoadApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RoadApp
+//
+//  Created by admin2 on 11/7/16.
+//  Copyright © 2016 admin2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RoadApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RoadApp
+//
+//  Created by admin2 on 11/7/16.
+//  Copyright © 2016 admin2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

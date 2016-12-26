6c4473dc52b168af4f35de026708e4344db71a7a
fukemy <fukemy@yahoo.com>
2016-12-26 11:49:30 +0700

    no message

---
+++ RoadApp/Constant.h
@@ -0,0 +1,15 @@
+//
+//  Constant.h
+//  RoadApp
+//
+//  Created by admin2 on 12/26/16.
+//  Copyright © 2016 admin2. All rights reserved.
+//
+
+#ifndef Constant_h
+#define Constant_h
+
+#pragma URL
+#define LOGIN_URL @"http://khaosatdiachat.com:8887/api/users/login/dungdv/123456"
+
+#endif /* Constant_h */

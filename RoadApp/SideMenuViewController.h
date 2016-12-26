fc0b2f9418f92930e44e2a6f129c5c0f344552c6
fukemy <fukemy@yahoo.com>
2016-12-26 20:00:42 +0700

    no message

---
+++ RoadApp/SideMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  SideMenuViewController.h
+//  RoadApp
+//
+//  Created by admin2 on 12/26/16.
+//  Copyright © 2016 admin2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SideMenuViewController : UIViewController
+
+@end
---
+++ RoadApp/SideMenuViewController.m
@@ -0,0 +1,37 @@
+//
+//  SideMenuViewController.m
+//  RoadApp
+//
+//  Created by admin2 on 12/26/16.
+//  Copyright © 2016 admin2. All rights reserved.
+//
+
+#import "SideMenuViewController.h"
+
+@interface SideMenuViewController ()
+
+@end
+
+@implementation SideMenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

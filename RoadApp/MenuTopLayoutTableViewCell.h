328b3a438a0f47c0c52c49b7664c6cd972c11c36
fukemy <fukemy@yahoo.com>
2016-12-27 17:12:37 +0700

    no message

---
+++ RoadApp/MenuTopLayoutTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  MenuTopLayoutTableViewCell.h
+//  RoadApp
+//
+//  Created by admin2 on 12/27/16.
+//  Copyright © 2016 admin2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuTopLayoutTableViewCell : UITableViewCell
+@property (strong, nonatomic) IBOutlet UILabel *tfName;
+@property (strong, nonatomic) IBOutlet UIImageView *imvAvatar;
+
+@end
---
+++ RoadApp/MenuTopLayoutTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MenuTopLayoutTableViewCell.m
+//  RoadApp
+//
+//  Created by admin2 on 12/27/16.
+//  Copyright © 2016 admin2. All rights reserved.
+//
+
+#import "MenuTopLayoutTableViewCell.h"
+
+@implementation MenuTopLayoutTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
